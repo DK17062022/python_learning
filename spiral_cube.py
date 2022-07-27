4e9af2c3ac41c2b11c7b5e68487e231b820a06e7
DK17062022 <private170219@gmail.com>
2022-07-27 23:06:57 +0300

    working version for inputs > 1

--- spiral_cube.py
+++ spiral_cube.py
@@ -20,38 +20,65 @@ max_num = arr_size * arr_size
 while fill_num <= max_num:
     if fill_num == max_num:
         break
-    check_num = fill_num - ((arr_size - 1) * 3)
-    if check_num == 0:
-        step += 1
     if direction == 'right':
-        for col in range(step, arr_size - 1 - step, 1):
-            fill_num += 1
-            arr[row][col] = fill_num
-        direction = 'down'
-        col += 1
-        continue
-    if direction == 'down':
-        for row in range(step, arr_size - 1 - step, 1):
-            fill_num += 1
-            arr[row][col] = fill_num
-        direction = 'left'
-        row += 1
-        continue
-    if direction == 'left':
-        for col in range(arr_size - 1 - step, step, -1):
-            fill_num += 1
-            arr[row][col] = fill_num
-        direction = 'up'
-        col -= 1
-        continue
-    if direction == 'up':
-        for row in range(arr_size - 1 - step, step + 1, -1):
-            fill_num += 1
-            arr[row][col] = fill_num
-        direction = 'right'
-        row -= 1
-        continue
+        while col < arr_size:
+            if arr[row][col] != 0:
+                direction = 'down'
+                row += 1
+                col -= 1
+                break
+            elif col == arr_size - 1:
+                direction = 'down'
+                break
+            elif arr[row][col] == 0:
+                fill_num += 1
+                arr[row][col] = fill_num
+                col += 1
 
+    if direction == 'down':
+        while row < arr_size:
+            if arr[row][col] != 0:
+                direction = 'left'
+                row -= 1
+                col -= 1
+                break
+            elif row == arr_size - 1:
+                direction = 'left'
+                break
+            elif arr[row][col] == 0:
+                fill_num += 1
+                arr[row][col] = fill_num
+                row += 1
+
+    if direction == 'left':
+        while col >= 0:
+            if arr[row][col] != 0:
+                direction = 'up'
+                row -= 1
+                col += 1
+                break
+            elif col == 0:
+                direction = 'up'
+                break
+            elif arr[row][col] == 0:
+                fill_num += 1
+                arr[row][col] = fill_num
+                col -= 1
+
+    if direction == 'up':
+        while row >= 0:
+            if arr[row][col] != 0:
+                direction = 'right'
+                row += 1
+                col += 1
+                break
+            elif row == 0:
+                direction = 'right'
+                break
+            elif arr[row][col] == 0:
+                fill_num += 1
+                arr[row][col] = fill_num
+                row -= 1
 
 # print(arr)
 for row in range(len(arr)):
